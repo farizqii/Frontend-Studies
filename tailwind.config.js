c5605f7da1a15c20e9e457ca89795e33e70e0760
farizqii <Farizqi122@gmail.com>
2026-07-02 18:22:17 +0700

    Changed videos into external youtube links

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./*.{html,js}"],
+  content: ["./*.html"], // This tells Tailwind to watch all HTML files in the root
     theme: {
     extend: {},
 },
